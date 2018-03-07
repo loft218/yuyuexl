6574ca6cd27eebc463349afdb816f061951618de
wangyi <lifeonfoot@qq.com>
2018-03-07 14:41:21 +0800

    调整项目结构

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './index.css';
+import './assets/index.css';
 
 import App from './App';
 import AdminIndex from './admin/Index'
