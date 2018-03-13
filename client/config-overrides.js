5f8793bfcecad1230290f2802f4df869fbaabd8d
wangyi <lifeonfoot@qq.com>
2018-03-13 18:16:34 +0800

    cra mutil pages

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,5 +2,29 @@ const rewireCssModules = require('react-app-rewire-css-modules');
 
 module.exports = function override(config, env) {
     config = rewireCssModules(config, env);
+
+    // config.entry = {
+    //     index: [
+    //         require.resolve('react-dev-utils/webpackHotDevClient'),
+    //         require.resolve('./polyfills'),
+    //         require.resolve('react-error-overlay'),
+    //         paths.appIndexJs,
+    //     ]
+    // }
+
+    // config.output = {
+    //     path: paths.appBuild,
+    //     pathinfo: true,
+    //     filename: 'static/js/[name].bundle.js',
+    //     chunkFilename: 'static/js/[name].chunk.js',
+    //     publicPath: publicPath,
+    //     devtoolModuleFilenameTemplate: info =>
+    //         path.resolve(info.absoluteResourcePath),
+    // }
+
+    
+
+    console.log(config.entry)
+
     return config;
 }
